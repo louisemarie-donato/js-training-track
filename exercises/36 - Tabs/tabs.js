29c9abb6b1d5d4db2366f815a021ee345f28b84d
Louise Marie Donato <louise.marie.donato@codeandtheory.com>
2022-03-07 17:33:02 +0800

    JS-17: Tabs

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,27 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+function clickTab(event) {
+  // hide tab panels
+  tabPanels.forEach((panel) => {
+    panel.hidden = true;
+  });
+
+  // unselect all tabs
+  tabButtons.forEach((tab) => {
+    tab.setAttribute('aria-selected', false);
+  });
+
+  // select clicked tab
+  event.currentTarget.setAttribute('aria-selected', true);
+
+  // show panel of selected tab
+  const selectedTab = tabPanels.find(
+    (panel) => panel.getAttribute('aria-labelledby') === event.target.id
+  );
+  console.log(selectedTab);
+  selectedTab.hidden = false;
+}
+
+tabButtons.forEach((button) => button.addEventListener('click', clickTab));
